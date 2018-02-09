240e5e7e6a2018bcd90b400186f584951b2c1ba9
Devin Parkinson <dparky13@icloud.com>
2018-02-09 14:40:17 -0700

    made alterations

--- answers.rb
+++ answers.rb
@@ -1,5 +1,21 @@
+ require 'colorize'
 class Answers
   def initialize(question)
-Answers = ["Yes", "no", "maybe", "why are you asking"]
-end
+  @answers ['Yes', 'No', 'Maybe', 'Try again later', 'Nice try', 'Possibly']
+  def menu
+    puts "Think of your question\nWhen ready type shake\nIf you want to leave, Type Quit"
+    shake = gets.strip.downcase
+    if shake == 'shake'
+    answers
+  elsif shake == 'quit'
+    exit
+    else
+      puts "Incorrect input, Please try again"
+      menu
+    end
+  end
+
+  def answers
+    @answers.rand {|a| puts a}
+  end
 end
